aea02f9bca8a9210e54d0d61d4093d1d77edafec
James Stine <James Stine>
2022-09-29 13:18:08 -0500

    Update

--- char/liberate/SCRIPTS/getname.tcl
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/tclsh
-puts "gf180mcu_$env(VARIANT)_$env(CORNER)_[string map {. P} $env(VLTG)]_$env(TEMP)C[string map {- . " " ""} $env(ARGS)]"
--- char/liberate/SCRIPTS/lib_to_db.tcl
+++
@@ -1,3 +0,0 @@
-read_lib $env(NAME).lib
-write_lib $env(NAME) -output $env(NAME).db
-quit
