d03ee5cfc7411124a8218f4958fcc51c113dfdbe
Landon Burleson <landonburleson203@gmail.com>
2022-10-06 09:16:33 -0500

    Removed unnecessary files.

--- flow/platforms/gf180mcu/make_tracks.tcl
+++
@@ -1,5 +0,0 @@
-make_tracks metal1 -x_offset 0.28 -x_pitch 0.56 -y_offset 0.28 -y_pitch 0.56
-make_tracks metal2 -x_offset 0.28 -x_pitch 0.56 -y_offset 0.28 -y_pitch 0.56
-make_tracks metal3 -x_offset 0.28 -x_pitch 0.56 -y_offset 0.28 -y_pitch 0.56
-make_tracks metal4 -x_offset 0.28 -x_pitch 0.56 -y_offset 0.28 -y_pitch 0.56
-make_tracks metal5 -x_offset 0.45 -x_pitch 0.90 -y_offset 0.45 -y_pitch 0.90
--- flow/tutorials/scripts/metal_fill/helpers.tcl
+++
@@ -1,1 +0,0 @@
-../drt/helpers.tcl
